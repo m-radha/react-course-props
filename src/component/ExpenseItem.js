1bd06263444dd1c54a25ed08123e5f0c36d5708b
Radha M <radha.m@tavant.com>
2023-10-04 14:34:30 +0530

    2nd commit

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import '../css/expenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
-  return (
-    <div className='expense-container'>
-      <p><b>{props.date.toISOString()}</b></p>
-      <h2 className='expense-content'><b>{props.title}</b></h2>
-      <h2 className='expense-price'>${props.amount}</h2>
-    </div>
+
+  return(
+    <div className="expense-container">
+      <ExpenseDate date={props.date}/>
+    <h2 className="expense-content">
+      <b>{props.title}</b>
+    </h2>
+    <h2 className="expense-price">${props.amount}</h2>
+  </div>
   )
 }
 
