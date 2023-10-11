311126255bbdee1424daadefaa96e4580692fc29
Radha M <radha.m@tavant.com>
2023-10-11 11:35:41 +0530

    committed

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import './Expenses.css';
+import ExpenseFilter from './ExpenseFilter';
+
+const Expenses = (props) => {
+  const [filteredYear,setFilteredYear] = useState('2020');
+  const [Infotext,setInfoText] = useState('2019,2021 & 2022');
+
+  
+
+  const HandleFilterHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+    if(selectedYear === '2019'){
+      setInfoText('2020,2021 & 2022')
+    }
+    else if(selectedYear === '2020'){
+      setInfoText('2019,2021 & 2022')
+    }
+    else if(selectedYear === '2021'){
+      setInfoText('2019,2020 & 2022')
+    }
+    else{
+      setInfoText('2019,2020 & 2021')
+    }
+  }
+
+  const FilteredExpenses = props.items.filter ((expense)=>{
+    return expense.date.getFullYear().toString() === filteredYear;
+  }) 
+  return (
+    <Card className="expenses">
+      <ExpenseFilter onSelectedYr={HandleFilterHandler}/>
+      <p style={{color:'white'}}>This yrs {Infotext} text was hidden.</p>
+      {FilteredExpenses.map((expense)=>(
+        // <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>
+        <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>
+      ))}
+    </Card>
+  );
+}
+
+export default Expenses;
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+
+import "./ExpenseForm.css";
+
+const ExpenseForm = (props) => {
+  const [EnteredTitle, setEnteredTitle] = useState("");
+  const [EnteredAmount, setEnteredAmount] = useState("");
+  const [EnteredDate, setEnteredDate] = useState("");
+
+  const HanadleTitleChange = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const HanadleAmountChange = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const HanadleDateChange = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+
+  // const HandleInputChange = (identifier,value) => {
+  //      if(identifier === 'title'){
+  //       setEnteredTitle(value);
+  //      }else if(identifier === 'amount'){
+  //       setEnteredAmount(value);
+  //      }else{
+  //       setEnteredDate(value);
+  //      }
+  // }
+
+  const HandleSubmit = (event) =>{
+    event.preventDefault();
+
+    const expenseData = {
+      title:EnteredTitle,
+      amount:EnteredAmount,
+      date:new Date(EnteredDate)
+    }
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('')
+  }
+
+
+  return (
+    <form onSubmit={HandleSubmit}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" value={EnteredTitle} onChange={HanadleTitleChange}/>
+          {/* <input type="text" onChange={(event)=>HandleInputChange('title',event.target.value)}/>  */}
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" min="0.01" step="0.01"  value={EnteredAmount} onChange={HanadleAmountChange}/>
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" min="2019-01-01" max="2022-12-31" value={EnteredDate} onChange={HanadleDateChange}/>
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
